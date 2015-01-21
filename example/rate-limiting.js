1388af36bbc9cb48d9c5d323a3a43e4ff738ebef
Raymond Feng <raymond@strongloop.com>
2015-01-20 16:05:34 -0800

    Add more comments

--- example/rate-limiting.js
+++ example/rate-limiting.js
@@ -4,30 +4,37 @@ var nools = require('nools');
 
 var app = express();
 
+// First define object types for facts. There will be LoopBack models.
+
+// Mock up the Context model
 var Context = function(req, res) {
   this.req = req;
   this.res = res;
 };
 
-// Define object types. We'll use LoopBack models!
+// Mock up the Application model
 var Application = function(id, name) {
   this.id = id;
   this.name = name;
 };
 
+// Mock up the User model
 var User = function(id, username, email) {
   this.id = id;
   this.username = username;
   this.email = email;
 };
 
+// Mock up the Status model
 var Status = function(count, limit) {
   this.count = count;
   this.limit = limit;
 };
 
+// A global counters
 var counters = {};
 
+// Define rules
 var flow = nools.flow("Rate Limiting", function(f) {
   this.rule("Limit reqs for application and user",
     [
@@ -60,12 +67,16 @@ var flow = nools.flow("Rate Limiting", function(f) {
 });
 
 function testFlow(ctx) {
+  // Create a session with initial facts
   var session = flow.getSession(new Status(0, 10));
 
+  // Add more facts
   session.assert(new Application(ctx.application.id,
     ctx.application.name));
   session.assert(new User(ctx.user.id, ctx.user.username,
     ctx.user.email));
+
+  // Match
   session.match().then(function() {
     var facts = session.getFacts();
     console.log('Facts:', facts);
@@ -80,6 +91,7 @@ function testFlow(ctx) {
   return session;
 }
 
+// Mock up the context setup
 app.use(function contextInit(req, res, next) {
   req.ctx = new Context(req, res);
   req.ctx.application = {
@@ -94,6 +106,7 @@ app.use(function contextInit(req, res, next) {
   next();
 });
 
+// Intercept the requests and enforce rules
 app.use(function(req, res, next) {
   testFlow(req.ctx);
 });
