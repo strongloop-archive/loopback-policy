b8870d09f0e9acfef59ca28920c7294c1a449d9e
Ryan Graham <r.m.graham@gmail.com>
2016-05-06 12:26:21 -0700

    update copyright notices and license

--- test/policy.test.js
+++ test/policy.test.js
@@ -1,2 +1,7 @@
+// Copyright IBM Corp. 2014,2015. All Rights Reserved.
+// Node module: loopback-policy
+// This file is licensed under the MIT License.
+// License text available at https://opensource.org/licenses/MIT
+
 describe('Rule', function() {
 });
